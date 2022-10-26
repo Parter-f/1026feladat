3b37d69858c239e8fc5363ed1cdc010b88a9d8cf
Árpás Péter <arpaspeter0@gmail.com>
2022-10-26 14:29:24 +0200

    mégegy commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function ellenorzes(szoveg , regex){
+
+    let helyes = false;
+    helyes = regex.test(szoveg);
+    return helyes
+
+}
+
+document.getElementById('fizetbutton').addEventListener('click' , () =>{
+
+let namePattern = /^[a-zA-Z]+$/;
+
+
+
+})
